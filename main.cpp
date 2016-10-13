cb55d44f1bb14cb48db9a1d49df8c29dc62a72ec
Alin G. Chitu <alin.chitu@tno.nl>
2016-10-13 09:54:30 +0200

    Small change for readability
    Only cosmetics

--- main.cpp
+++ main.cpp
@@ -25,7 +25,6 @@
  * @brief This file contains the main function for the stand-alone well index calculator executable.
  */
 
-
 #include "main.hpp"
 #include "wellindexcalculator.h"
 #include <Reservoir/grid/eclgrid.h>
@@ -35,6 +34,7 @@ using namespace std;
 int main(int argc, const char *argv[]) {
     // Initialize some variables from the runtime arguments
     auto vm = createVariablesMap(argc, argv);
+	
     auto heel = Eigen::Vector3d(vm["heel"].as<vector<double>>().data());
     auto toe = Eigen::Vector3d(vm["toe"].as<vector<double>>().data());
     string gridpth = vm["grid"].as<string>();
@@ -42,9 +42,9 @@ int main(int argc, const char *argv[]) {
 
     // Initialize the Grid and WellIndexCalculator objects
     auto grid = new Reservoir::Grid::ECLGrid(gridpth);
-    auto wic = WellIndexCalculator(grid);
-
+    
     // Compute the well blocks
+    auto wic = WellIndexCalculator(grid);
     auto well_blocks = wic.ComputeWellBlocks(heel, toe, wellbore_radius);
 
     if (vm.count("compdat")) { // Print as a COMPDAT table if the --compdat/-c flag was given
@@ -54,5 +54,6 @@ int main(int argc, const char *argv[]) {
     else { // Otherwise, print as a CSV table
         printCsv(well_blocks);
     }
+    
     return 0;
 }
