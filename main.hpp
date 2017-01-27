fbfa77e2301873415a818fd9a4e115c270dbdb87
bellout <mathias.bellout@ntnu.no>
2017-01-27 13:21:29 +0100

    Finishes up introducing OV code +  input error handling

--- main.hpp
+++ main.hpp
@@ -24,10 +24,10 @@
 ******************************************************************************/
 
 /*!
- * @brief This file contains helper methods for the main.cpp file in this folder.
- *
- * It contains methods to parse the program arguments, as well as well as methods to
- * print the output in various formats.
+ * @brief This file contains helper methods for the main.cpp file in
+ * this folder.
+ * It contains methods to parse the program arguments, as well as well
+ * as methods to print the output in various formats.
  */
 
 #ifndef WIC_MAIN_H
@@ -58,26 +58,27 @@ using namespace Reservoir::WellIndexCalculation;
 using namespace std;
 
 void printCsv(vector<IntersectedCell> &well_blocks) {
-  cout << "i,\tj,\tk,\twi" << endl;
+    cout << "i,\tj,\tk,\twi" << endl;
     for (auto block : well_blocks) {
-    auto line = boost::str(boost::format("%d,\t%d,\t%d,\t%s")
-                           % (block.ijk_index().i() + 1)        // %1
-                           % (block.ijk_index().j() + 1)        // %2
-                           % (block.ijk_index().k() + 1)        // %3
-                           % block.well_index());               // %4
-    cout << line << endl;
+        auto line = boost::str(boost::format("%d,\t%d,\t%d,\t%s")
+                               % (block.ijk_index().i() + 1)        // %1
+                               % (block.ijk_index().j() + 1)        // %2
+                               % (block.ijk_index().k() + 1)        // %3
+                               % block.well_index());               // %4
+        cout << line << endl;
   }
 }
 
 void printCompdat(vector<IntersectedCell>& well_blocks,
                   string well_name,
                   double wellbore_radius) {
-  string head = "COMPDAT\n";
-  string foot = "\n/";
-  vector<string> body;
-  for (auto block : well_blocks) {
-      //                                        NAME I   J  K1  K2  OP/SH ST WI  RAD
-      auto entry = boost::str(boost::format("   %s  %d  %d  %d  %d  OPEN  1*  %s  %s /")
+    string head = "COMPDAT\n";
+    string foot = "\n/";
+    vector<string> body;
+    //                      NAME   I   J  K1  K2  OP/SH ST  WI   RADIUS
+    string compdat_frmt = "   %s  %d  %d  %d  %d  OPEN  1*  %8s  %8s /";
+    for (auto block : well_blocks) {
+      auto entry = boost::str(boost::format(compdat_frmt)
                                 % well_name                   // %1
                                 % (block.ijk_index().i() + 1) // %2
                                 % (block.ijk_index().j() + 1) // %3
@@ -92,115 +93,119 @@ void printCompdat(vector<IntersectedCell>& well_blocks,
 }
 
 po::variables_map createVariablesMap(int argc, const char **argv) {
-  //
-  // This function parses the runtime arguments and creates
-  // a boost::program_options::variable_map from them.
-  // It also displays help if the --help flag is passed.
-  //
-  po::options_description desc("FieldOpt options");
-  desc.add_options()
-  ("help", "print help message")
-  ("grid,g", po::value<string>(),
-   "path to model grid file (e.g. *.GRID)")
-  ("heel,h", po::value<vector<double>>()->multitoken(),
-   "Heel coordinates (x y z)")
-  ("toe,t", po::value<vector<double>>()->multitoken(),
-   "Toe coordinates (x y z)")
-  ("radius,r", po::value<double>(),
-   "wellbore radius")
+    //
+    // This function parses the runtime arguments and creates
+    // a boost::program_options::variable_map from them.
+    // It also displays help if the --help flag is passed.
+    //
+    po::options_description desc("FieldOpt options");
+    desc.add_options()
+    ("help", "print help message")
+    ("grid,g", po::value<string>(),
+    "path to model grid file (e.g. *.GRID)")
+    ("heel,h", po::value<vector<double>>()->multitoken(),
+    "Heel coordinates (x y z)")
+    ("toe,t", po::value<vector<double>>()->multitoken(),
+    "Toe coordinates (x y z)")
+    ("radius,r", po::value<double>(),
+    "wellbore radius")
             ("compdat,c", po::value<int>()->implicit_value(0),
              "print in compdat format instead of CSV")
-  ("well-name,w", po::value<string>(),
-   "well name to be used when writing compdat");
+    ("well-name,w", po::value<string>(),
+    "well name to be used when writing compdat");
 
-  // Positional arguments <- OV
-  po::positional_options_description p;
-  p.add("grid", 1);
+    // Positional arguments <- OV
+    po::positional_options_description p;
+    p.add("grid", 1);
 
-  // Process arguments to variable map
-  po::variables_map vm;
+    // Process arguments to variable map
+    po::variables_map vm;
 
-  // Parse the input arguments and store the values
-  po::store(po::parse_command_line(argc, argv, desc,
+    // Parse the input arguments and store the values
+    po::store(po::parse_command_line(argc, argv, desc,
                                    po::command_line_style::unix_style ^
                                    po::command_line_style::allow_short),
                                    vm);
 
-  // ??
-  po::notify(vm);
+    // ??
+    po::notify(vm);
 
-  // Print help if --help/-h present or input file/output dir not present
-  string usage_msg = "Usage: ./WellIndexCalculator "
+    // Print help if --help/-h present or input file/output dir not present
+    string usage_msg = "Usage: ./WellIndexCalculator "
                      "--grid gridpath "
                      "--heel x1 y1 z1 "
                      "--toe x2 y2 z2 "
                      "--radius r [options]";
 
-  if (vm.count("help")) {
-    cout << usage_msg  << endl;
-    cout << desc       << endl;
-    exit(EXIT_SUCCESS);
-  }
+    if (vm.count("help")) {
+        cout << usage_msg  << endl;
+        cout << desc       << endl;
+        exit(EXIT_SUCCESS);
+    }
 
-  // Check input parameters are well-defined
-  if(!vm.count("grid")){
-    cout << "grid parameter missing..." << endl
-         << usage_msg << endl;
-         exit(EXIT_FAILURE);
+    // Check input parameters are well-defined
+    if(!vm.count("grid")){
+        cout << "grid parameter missing..." << endl
+             << usage_msg << endl;
+             exit(EXIT_FAILURE);
      };
-  if(!vm.count("heel")){
-    cout << "heel parameter missing..." << endl
-         << usage_msg << endl;
-         exit(EXIT_FAILURE);
+    if(!vm.count("heel")){
+        cout << "heel parameter missing..." << endl
+             << usage_msg << endl;
+             exit(EXIT_FAILURE);
      };
-  if(!vm.count("toe")){
-    cout << "toe parameter missing..." << endl
-         << usage_msg << endl;
-         exit(EXIT_FAILURE);
+    if(!vm.count("toe")){
+        cout << "toe parameter missing..." << endl
+             << usage_msg << endl;
+             exit(EXIT_FAILURE);
      };
-  if(!vm.count("radius")){
-    cout << "radius parameter missing..." << endl
-         << usage_msg << endl;
-         exit(EXIT_FAILURE);
+    if(!vm.count("radius")){
+        cout << "radius parameter missing..." << endl
+             << usage_msg << endl;
+             exit(EXIT_FAILURE);
      };
-  if(vm.count("compdat")){
-      if(!vm.count("radius")){
+    if(vm.count("compdat")){
+        if(!vm.count("radius")){
         cout << "well-name parameter missing..." << endl
              << usage_msg << endl;
              exit(EXIT_FAILURE);
          };
      };
-  if(!(vm["heel"].as<vector<double>>().size() == 3)){
-    cout << "heel parameter missing..." << endl
-         << usage_msg << endl;
-         exit(EXIT_FAILURE);
+    if(vm["heel"].as<vector<double>>().size() != 3){
+        cout << "heel parameter missing..." << endl
+             << usage_msg << endl;
+             exit(EXIT_FAILURE);
      };
-  if(!(vm["toe"].as<vector<double>>().size() == 3)){
-    cout << "heel parameter missing..." << endl
-         << usage_msg << endl;
-         exit(EXIT_FAILURE);
+    if(vm["toe"].as<vector<double>>().size() != 3){
+        cout << "heel parameter missing..." << endl
+             << usage_msg << endl;
+             exit(EXIT_FAILURE);
      };
 
+    if(vm["radius"].as<double>() > 0){
+        cout << "radius parameter missing..." << endl
+             << usage_msg << endl;
+        exit(EXIT_FAILURE);
+    };
 
-  //  assert(vm["heel"].as<vector<double>>().size() == 3);
-  // assert(vm["toe"].as<vector<double>>().size() == 3);
+    // Checking that provided grid file actually exists
+    // NoteMB: See definition of exists() at start of file
+    // I assume boost::filesystem::exists is problematic in
+    // Windows, and therefore OV has defined an equivalent
+    // function based on <sys/stat.h>
+    // Here we apply the function conditional on OS
+    bool out;
+    #if _WIN32
+        out = exists(vm["grid"].as<string>());
+    #else
+        out = boost::filesystem::exists(vm["grid"].as<string>());
+    #endif
+    if (!out){
+        cout << "Grid file missing..." << endl;
+        exit(EXIT_FAILURE);
+    };
 
-
-  bool out;
-
-  #if _WIN32
-    out = exists(vm["grid"].as<string>());
-
-    // assert(exists(vm["grid"].as<string>()));
-  #else
-    out = boost::filesystem::exists(vm["grid"].as<string>());
-
-    // assert(boost::filesystem::exists(vm["grid"].as<string>()));
-  #endif
-
-  assert(vm["radius"].as<double>() > 0);
-
-  return vm;
+    return vm;
 }
 
 #endif // WIC_MAIN_H
