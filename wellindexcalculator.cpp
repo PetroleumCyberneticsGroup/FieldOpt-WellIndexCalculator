4b6d07191b773a94528b2c14bb4f76aad574d423
bellout <mathias.bellout@ntnu.no>
2017-01-28 15:32:02 +0100

    Formatting

--- wellindexcalculator.cpp
+++ wellindexcalculator.cpp
@@ -27,32 +27,39 @@
 
 namespace Reservoir {
     namespace WellIndexCalculation {
+        using namespace std;
+
         WellIndexCalculator::WellIndexCalculator(Grid::Grid *grid) {
             grid_ = grid;
         }
 
-        std::vector<IntersectedCell> WellIndexCalculator::ComputeWellBlocks(Vector3d heel, Vector3d toe, double wellbore_radius) {
-            heel_ = heel;
-            toe_ = toe;
+        vector<IntersectedCell>
+        WellIndexCalculator::ComputeWellBlocks(Vector3d heel,
+                                                Vector3d toe,
+                                                double wellbore_radius) {
+                       heel_ = heel;
+                        toe_ = toe;
             wellbore_radius_ = wellbore_radius;
+            vector<IntersectedCell> intersected_cells = cells_intersected();
 
-            std::vector<IntersectedCell> intersected_cells = cells_intersected();
-	    
             for (int i = 0; i < intersected_cells.size(); ++i) {
-                intersected_cells[i].set_well_index(compute_well_index(intersected_cells[i]));
+                intersected_cells[i].set_well_index(
+                        compute_well_index( intersected_cells[i] )
+                );
             }
             return intersected_cells;
         }
 
-        std::vector<IntersectedCell> WellIndexCalculator::cells_intersected() {
-            
-	    std::vector<IntersectedCell> intersected_cells;
-	    
-	    // Find the heel cell and add it to the list
-            intersected_cells.push_back(IntersectedCell(grid_->GetCellEnvelopingPoint(heel_)));
+        vector<IntersectedCell> WellIndexCalculator::cells_intersected() {
+	        vector<IntersectedCell> intersected_cells;
+
+	        // Find the heel cell and add it to the list
+            intersected_cells.push_back(
+                    IntersectedCell( grid_->GetCellEnvelopingPoint(heel_) )
+            );
             intersected_cells[0].set_entry_point(heel_);
-	    
-	    // Find the toe cell
+
+	        // Find the toe cell -- removed by OV; why?
             Grid::Cell last_cell = grid_->GetCellEnvelopingPoint(toe_);
 
             // If the first and last blocks are the same, return the block and start+end points
@@ -62,20 +69,31 @@ namespace Reservoir {
             }
 
             // Make sure we follow line in the correct direction. (i.e. dot product positive)
-            Vector3d exit_point = find_exit_point(intersected_cells[0], heel_, toe_, heel_);
+            Vector3d exit_point;
+            exit_point = find_exit_point(intersected_cells[0],
+                                         heel_,
+                                         toe_,
+                                         heel_);
+
             if ((toe_ - heel_).dot(exit_point - heel_) <= 0.0) {
-                exit_point = find_exit_point(intersected_cells[0], heel_, toe_, exit_point);
+                exit_point = find_exit_point(intersected_cells[0],
+                                             heel_,
+                                             toe_,
+                                             exit_point);
             }
             intersected_cells[0].set_exit_point(exit_point);
 
             double epsilon = 0.01 / (toe_ - exit_point).norm();
 
-            // Add previous exit point to list, find next exit point and all other up to the end_point    
-            while (true) {		
+            // Add previous exit point to list, find next exit point and all other up to the end_point
+            while (true) {
                 // Move into the next cell, add it to the list and set the entry point
                 Vector3d move_exit_epsilon = exit_point * (1 - epsilon) + toe_ * epsilon;
-                intersected_cells.push_back(IntersectedCell(grid_->GetCellEnvelopingPoint(move_exit_epsilon)));
-                intersected_cells.back().set_entry_point(exit_point); // The entry point of each cell is the exit point of the previous cell
+                intersected_cells.push_back(
+                        IntersectedCell(grid_->GetCellEnvelopingPoint( move_exit_epsilon ))
+                );
+                // The entry point of each cell is the exit point of the previous cell
+                intersected_cells.back().set_entry_point(exit_point);
 
                 // Terminate if we're in the last cell
                 if (intersected_cells.back().global_index() == last_cell.global_index()) {
@@ -84,25 +102,32 @@ namespace Reservoir {
                 }
 
                 // Find the exit point of the cell and set it in the list
-                exit_point = find_exit_point(intersected_cells.back(), exit_point, toe_, exit_point);
+                exit_point = find_exit_point(intersected_cells.back(),
+                                             exit_point,
+                                             toe_,
+                                             exit_point);
                 intersected_cells.back().set_exit_point(exit_point);
-                assert(intersected_cells.size() < 500);		
+                assert(intersected_cells.size() < 500);
             }
-	    
+
             assert(intersected_cells.back().global_index() == last_cell.global_index());
             return intersected_cells;
         }
 
-        Vector3d WellIndexCalculator::find_exit_point(Grid::Cell &cell, Vector3d &entry_point,
-                                                      Vector3d &end_point, Vector3d &exception_point) {
+        Vector3d WellIndexCalculator::find_exit_point(Grid::Cell &cell,
+                                                      Vector3d &entry_point,
+                                                      Vector3d &end_point,
+                                                      Vector3d &exception_point) {
             Vector3d line = end_point - entry_point;
 
             // Loop through the cell faces until we find one that the line intersects
             for (Grid::Cell::Face face : cell.faces()) {
-                if (face.normal_vector.dot(line) != 0) { // Check that the line and face are not parallel.
+                if (face.normal_vector.dot(line) != 0) {
+                    // Check that the line and face are not parallel.
                     auto intersect_point = face.intersection_with_line(entry_point, end_point);
 
-                    // Check that the intersect point is on the correct side of all faces (i.e. inside the cell)
+                    // Check that the intersect point is on the correct
+                    // side of all faces (i.e. inside the cell)
                     bool feasible_point = true;
                     for (auto p : cell.faces()) {
                         if (!p.point_on_same_side(intersect_point, 10e-6)) {
@@ -111,14 +136,20 @@ namespace Reservoir {
                         }
                     }
 
-                    // Return the point if it is deemed feasible, not identical to the entry point, and going in the correct direction.
-                    if (feasible_point && (exception_point - intersect_point).norm() > 10e-10
-                        && (end_point - entry_point).dot(end_point - intersect_point) >= 0) {
+                    // Return intersection point if:
+                    // it is deemed feasible:
+                    auto cond_a = feasible_point;
+                    // + it is not identical to entry point
+                    auto cond_b = (exception_point - intersect_point).norm() > 10e-10;
+                    // + it is going in the correct direction.
+                    auto cond_c = (end_point - entry_point).dot(end_point - intersect_point) >= 0;
+                    if (cond_a && cond_b && cond_c) {
                         return intersect_point;
                     }
                 }
             }
-            // If all fails, the line intersects the cell in a single point (corner or edge) -> return entry_point
+            // If all fails, the line intersects the cell in a
+            // single point (corner or edge) -> return entry_point
             return entry_point;
         }
 
@@ -131,7 +162,8 @@ namespace Reservoir {
                 // Compute vector from segment
                 Vector3d current_vec = icell.points().at(ii+1) - icell.points().at(ii);
 
-                /* Projects segment vector to directional spanning vectors and determines the length.
+                /*
+                 * Projects segment vector to directional spanning vectors and determines the length.
                  * of the projections. Note that we only only care about the length of the projection,
                  * not the spatial position. Also adds the lengths of previous segments in case there
                  * is more than one segment within the well.
@@ -141,22 +173,27 @@ namespace Reservoir {
                 Lz = Lz + (icell.zvec() * icell.zvec().dot(current_vec) / icell.zvec().dot(icell.zvec())).norm();
             }
 
-            // Compute Well Index from formula provided by Shu
+            // Compute Well Index from formula provided by Shu (\todo Introduce ref/year)
             double well_index_x = (dir_well_index(Lx, icell.dy(), icell.dz(), icell.permy(), icell.permz()));
             double well_index_y = (dir_well_index(Ly, icell.dx(), icell.dz(), icell.permx(), icell.permz()));
             double well_index_z = (dir_well_index(Lz, icell.dx(), icell.dy(), icell.permx(), icell.permy()));
-            double wi = sqrt(well_index_x * well_index_x + well_index_y * well_index_y + well_index_z * well_index_z);
+
+            double wi = sqrt(well_index_x * well_index_x +
+                             well_index_y * well_index_y +
+                             well_index_z * well_index_z);
             return wi;
         }
 
-        double WellIndexCalculator::dir_well_index(double Lx, double dy, double dz, double ky, double kz) {
+        double WellIndexCalculator::dir_well_index(double Lx, double dy,
+                                                   double dz, double ky, double kz) {
             double silly_eclipse_factor = 0.008527;
             double well_index_i = silly_eclipse_factor * (2 * M_PI * sqrt(ky * kz) * Lx) /
                                   (log(dir_wellblock_radius(dy, dz, ky, kz) / wellbore_radius_));
             return well_index_i;
         }
 
-        double WellIndexCalculator::dir_wellblock_radius(double dx, double dy, double kx, double ky) {
+        double WellIndexCalculator::dir_wellblock_radius(double dx, double dy,
+                                                         double kx, double ky) {
             double r = 0.28 * sqrt((dx * dx) * sqrt(ky / kx) + (dy * dy) * sqrt(kx / ky)) /
                        (sqrt(sqrt(kx / ky)) + sqrt(sqrt(ky / kx)));
             return r;
