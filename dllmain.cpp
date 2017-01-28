bcd3c643dfb05742c4fcd28c9211d4d63aa02912
bellout <mathias.bellout@ntnu.no>
2017-01-28 15:31:10 +0100

    Files needed for ADGPRS plugin compilation

---
+++ dllmain.cpp
@@ -0,0 +1,135 @@
+
+#include "dllmain.hpp"
+#include "wellindexcalculator.h"
+#include "Reservoir/grid/eclgrid.h"
+#include "intersected_cell.h"
+
+#include <stdio.h>
+#include <sys/stat.h>
+inline bool exists(const char* name)
+{
+  struct stat buffer; 
+  return (stat (name, &buffer) == 0);
+}
+
+using namespace Reservoir::WellIndexCalculation;
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+  switch (ul_reason_for_call)
+  {
+  case DLL_PROCESS_ATTACH:
+    grid = NULL;
+    if (lpReserved == NULL) // Dynamic load
+    {
+      // Initialize your stuff or whatever
+      // Return FALSE if you don't want your module to be dynamically loaded
+    }
+    else // Static load
+    {
+      // Return FALSE if you don't want your module to be statically loaded
+      return FALSE;
+    }
+    break;
+  case DLL_THREAD_ATTACH:
+  case DLL_THREAD_DETACH:
+  case DLL_PROCESS_DETACH:
+    break;
+  }
+  return TRUE;
+}
+
+// This is an example of an exported variable
+//WELLINDEXCALCULATOR_API Grid::Grid grid = NULL;
+
+// This is an example of an exported function.
+WELLINDEXCALCULATOR_API int computeWellIndices(const char* basepth,
+    const double* heel, const double* toe, const double* wellbore_radius,
+    int* nblks, int* i, int* j, int* k, double* wi)
+{
+  try
+  {
+    string gridpth = string(basepth) + ".GRID";
+    if ( !exists(gridpth.c_str()) )
+      throw std::runtime_error("ComputeWellIndices: file .GRID does not exist");
+
+    if ( *wellbore_radius <= 0 )
+      throw std::runtime_error("ComputeWellIndices: wellbore radius is negative");
+
+    // Initialize the Grid and WellIndexCalculator objects
+    if (grid == NULL)
+      grid = new Reservoir::Grid::ECLGrid(gridpth);
+    auto wic = WellIndexCalculator((Reservoir::Grid::Grid*)grid);
+
+    // Compute the well blocks
+    auto well_blocks = wic.ComputeWellBlocks(Eigen::Vector3d(heel), Eigen::Vector3d(toe), *wellbore_radius);
+    *nblks = well_blocks.size();
+
+    if ( (i == NULL) || (j == NULL) || (k == NULL) || (wi == NULL) )
+      throw std::runtime_error("ComputeWellIndices: allocate memory for I, J, K, WI");
+
+    for (size_t iblk = 0; iblk < *nblks; iblk++)
+    {
+      i[iblk] = well_blocks[iblk].ijk_index().i() + 1;
+      j[iblk] = well_blocks[iblk].ijk_index().j() + 1;
+      k[iblk] = well_blocks[iblk].ijk_index().k() + 1;
+      wi[iblk] = well_blocks[iblk].well_index();
+    }
+
+    //delete grid;
+  }
+  catch (std::runtime_error& e)
+  {
+    fprintf(stdout, "%s", e.what());
+    fflush(stdout);
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
+
+WELLINDEXCALCULATOR_API int computeBlockCenter(const char* basepth,
+    const int* heel,  const int* toe, double* heelxyz, double* toexyz)
+{
+  try
+  {
+    string gridpth = string(basepth) + ".GRID";
+    if ( !exists(gridpth.c_str()) )
+      throw std::runtime_error("ComputeBlockCenter: file .GRID does not exist");
+
+    // Initialize the Grid and WellIndexCalculator objects
+    if (grid == NULL)
+      grid = new Reservoir::Grid::ECLGrid(gridpth);
+
+    Reservoir::Grid::Cell current_cell =
+      ((Reservoir::Grid::Grid*)grid)->GetCell(heel[0] - 1, heel[1] - 1, heel[2] - 1);
+    heelxyz[0] = current_cell.center()[0];
+    heelxyz[1] = current_cell.center()[1];
+    heelxyz[2] = current_cell.center()[2];
+
+    current_cell = ((Reservoir::Grid::Grid*)grid)->GetCell(toe[0] - 1, toe[1] - 1, toe[2] - 1);
+    toexyz[0] = current_cell.center()[0];
+    toexyz[1] = current_cell.center()[1];
+    toexyz[2] = current_cell.center()[2];
+
+    //delete grid;
+  }
+  catch (std::runtime_error& e)
+  {
+    printf("%s", e.what());
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
+
+// This is the constructor of a class that has been exported.
+// see WellIndexCalculator.h for the class definition
+//CWellIndexCalculator::CWellIndexCalculator()
+//{
+//  return;
+//}
+
---
+++ dllmain.hpp
@@ -0,0 +1,56 @@
+// dllmain.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+// Including SDKDDKVer.h defines the highest available Windows platform.
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
+
+#include <SDKDDKVer.h>
+
+#define WIN32_LEAN_AND_MEAN   // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+// The following ifdef block is the standard way of creating macros which make exporting
+// from a DLL simpler. All files within this DLL are compiled with the WELLINDEXCALCULATOR_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see
+// WELLINDEXCALCULATOR_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef WELLINDEXCALCULATOR_EXPORTS
+#ifdef __cplusplus
+#define WELLINDEXCALCULATOR_API  extern "C" __declspec(dllexport)
+#else
+#define WELLINDEXCALCULATOR_API  __declspec(dllexport)
+#endif
+#else
+#ifdef __cplusplus
+#define WELLINDEXCALCULATOR_API extern "C" __declspec(dllimport)
+#else
+#define WELLINDEXCALCULATOR_API __declspec(dllimport)
+#endif
+#endif
+
+// This is the constructor of a class that has been exported.
+// This class is exported from the WellIndexCalculator.dll
+//class WELLINDEXCALCULATOR_API CWellIndexCalculator {
+//public:
+//  CWellIndexCalculator(void);
+// TODO: add your methods here.
+//};
+
+// This is an example of an exported variable
+void* grid;
+
+// This is an example of an exported function.
+WELLINDEXCALCULATOR_API int computeWellIndices(const char* gridpth,
+    const double* heel, const double* toe, const double* wellbore_radius,
+    int* n, int* i, int* j, int* k, double* wi);
+
+WELLINDEXCALCULATOR_API int computeBlockCenter(const char* gridpth,
+    const int* heel,  const int* toe, double* heelxyz, double* toexyz);
+
